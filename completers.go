4ebd87ea4d2c95382264031b652f945ecec7c3b9
Todd Neal <todd@tneal.org>
2016-09-23 21:32:28 -0500

    add file and directory only completers

--- completers.go
+++ completers.go
@@ -12,36 +12,60 @@ import (
 // the registered placeholder types.
 type Completer func(arg string) []string
 
-// CompleteFileOrDir is a pre-defined completer that is used to complete
+type completeMode byte
+
+const (
+	filesAndDirs completeMode = iota
+	filesOnly
+	dirsOnly
+)
+
+// CompleteFileOrDirectory is a pre-defined completer that is used to complete
 // a file or directory name.
-func CompleteFileOrDir(fileOrDir string) []string {
-	// if the user supplied no input, pre-populate with the cwd
-	if fileOrDir == "" {
-		fileOrDir = "./"
-	}
+var CompleteFileOrDirectory = completeFileDir(filesAndDirs)
 
-	base := path.Clean(fileOrDir)
-	dir, file := path.Split(base)
-	// path specifies a directory, so complete inside of it
-	if fs, err := os.Stat(fileOrDir); err == nil && fs.IsDir() {
-		dir = fileOrDir
-		file = ""
-	}
+// CompleteDirectory is a pre-defined completer that is used to complete
+// a directory name.
+var CompleteDirectory = completeFileDir(dirsOnly)
 
-	matchFiles, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return nil
-	}
-	cFiles := []string{}
-	for _, mf := range matchFiles {
-		if strings.HasPrefix(mf.Name(), file) {
-			cf := path.Join(dir, mf.Name())
-			if fs, err := os.Stat(cf); err == nil && fs.IsDir() {
-				cFiles = append(cFiles, cf+string(os.PathSeparator))
-			} else {
-				cFiles = append(cFiles, cf)
+// CompleteFile is a pre-defined completer that is used to complete
+// a file name.
+var CompleteFile = completeFileDir(filesOnly)
+
+func completeFileDir(mode completeMode) func(fileOrDir string) []string {
+	return func(fileOrDir string) []string {
+		// if the user supplied no input, pre-populate with the cwd
+		if fileOrDir == "" {
+			fileOrDir = "./"
+		}
+
+		base := path.Clean(fileOrDir)
+		dir, file := path.Split(base)
+		// path specifies a directory, so complete inside of it
+		if fs, err := os.Stat(fileOrDir); err == nil && fs.IsDir() {
+			dir = fileOrDir
+			file = ""
+		}
+
+		matchFiles, err := ioutil.ReadDir(dir)
+		if err != nil {
+			return nil
+		}
+		cFiles := []string{}
+		for _, mf := range matchFiles {
+			if strings.HasPrefix(mf.Name(), file) {
+				cf := path.Join(dir, mf.Name())
+				if fs, err := os.Stat(cf); err == nil && fs.IsDir() {
+					if mode == filesAndDirs || mode == dirsOnly {
+						cFiles = append(cFiles, cf+string(os.PathSeparator))
+					}
+				} else {
+					if mode == filesAndDirs || mode == filesOnly {
+						cFiles = append(cFiles, cf)
+					}
+				}
 			}
 		}
+		return cFiles
 	}
-	return cFiles
 }
